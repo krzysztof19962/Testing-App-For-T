5745e8518742b3e9c0193d4aa2900ac249cb0e3a
Krzysztof Kowalski <krzysztof96@poczta.fm>
2018-04-27 18:16:25 +0200

    First commit of testing App

--- Testing App/Window1.xaml.cs
+++ Testing App/Window1.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,6 +15,7 @@ using System.Windows.Shapes;
 
 namespace Testing_App
 {
+    
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
@@ -23,5 +25,40 @@ namespace Testing_App
         {
             InitializeComponent();
         }
+        public Window1(int liczba)
+        {
+            
+            InitializeComponent();
+           createformula(liczba);
+        }
+
+        public IList<Label> pytania;
+        public IList<TextBox> formularzpytania;
+        public IList<TextBox> odpowiedzi1;
+        public IList<TextBox> odpowiedzi2;
+        public IList<TextBox> odpowiedzi3;
+        public IList<RadioButton> radio1;
+        public IList<RadioButton> radio2;
+        public IList<RadioButton> radio3;
+        public void createformula(int liczba)
+        {
+            pytania = new List<Label>();
+            formularzpytania = new List<TextBox>();
+            for (int i = 0; i < liczba; i++)
+            {
+                // Important stuff
+                //StackPanel1.Children.Add(new CheckBox());
+                TextBox pytanie = new TextBox();
+               // ScrollViewer1.Content += pytanie;
+                pytanie.Text = "Udalo sie";
+               // StackPanel1.Children += pytanie;
+                StackPanel1.DataContext = pytanie.DataContext;
+
+                // Label pytanie = new Label();
+                //  RadioButton radioguzik = new RadioButton();
+                //  radioguzik.Content = pytanie.Content;
+            }
+        }
+
     }
 }
---
+++ Testing App/start.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Testing_App
+{
+    
+    /// <summary>
+    /// Interaction logic for start.xaml
+    /// </summary>
+    public partial class start : Window
+    {
+        public int liczbapytanint = 0;
+        public start()
+        {
+            InitializeComponent();
+        }
+
+        private void liczbapytan(object sender, RoutedEventArgs e)
+        {
+            //Int32.Parse(TextBoxzliczbapytan.Text);
+            liczbapytanint = Int32.Parse(textBoxzliczbapytan.Text);
+            Window1 okno1 = new Window1(liczbapytanint);
+            okno1.Show();
+            this.Close();
+
+        }
+
+       
+    }
+}
